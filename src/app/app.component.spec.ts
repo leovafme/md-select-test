53d627c35fb46a1912cc4193fad01c6d6cf85918
leovafme <leovafme@hotmail.com>
2017-01-25 21:49:43 -0500

    test ok md-select

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,12 +25,20 @@ describe('AppComponent', () => {
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
+  // it(`should have as title 'app works!'`, async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('app works!');
+  // }));
+
   it(`should have as title 'app works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app works!');
   }));
